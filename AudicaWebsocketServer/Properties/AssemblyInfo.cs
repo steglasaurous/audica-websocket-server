a6b0151fec3faebab79011fe4bc85edc110e2de6
Scott Teglasi <scott.teglasi@gmail.com>
2022-02-01 20:44:12 -0500

    Update version numbers

--- AudicaWebsocketServer/Properties/AssemblyInfo.cs
+++ AudicaWebsocketServer/Properties/AssemblyInfo.cs
@@ -33,10 +33,10 @@ using MelonLoader;
 // You can specify all the values or you can default the Build and Revision Numbers
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyFileVersion("1.0.0.0")]
+[assembly: AssemblyVersion("1.1.0.0")]
+[assembly: AssemblyFileVersion("1.1.0.0")]
 
 // FIXME: Use .BuildInfo.Name, Version, etc instead of hard-coding it here.
-[assembly: MelonInfo(typeof(AudicaWebsocketServer.AudicaWebsocketServerMain), "AudicaWebsocketServer", "1.0.0", "Steglasaurous")]
+[assembly: MelonInfo(typeof(AudicaWebsocketServer.AudicaWebsocketServerMain), "AudicaWebsocketServer", "1.1.0", "Steglasaurous")]
 // FIXME: Replace with the correct Audica definition, since this is NOT a universal mod. 
 [assembly: MelonGame(null, null)]
